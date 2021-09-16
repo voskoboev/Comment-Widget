01d8047f9a9b2ff90fe0dd53c35d12e4e198a4bd
Dmitry <dvoskoboev@gmail.com>
2021-09-16 15:58:01 +0300

    Changed styles of card and card creator.

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -12,16 +12,19 @@ const Comment = props => {
       <p className="comment__list-item--text">
         {text}
       </p>
-      <time className="comment__list-item--time">
-        {new Date(date).toLocaleDateString()},
-        {new Date(date).toLocaleTimeString()}
-      </time>
-      <button
-        className="comment__list-item--delete-btn"
-        onClick={() => props.deleteComment(id)}
-      >
-        Delete
-      </button>
+      <div className="comment__list-item-wrapper">
+        <div className="comment__list-item--time">
+          {new Date(date).toLocaleDateString()},
+          {new Date(date).toLocaleTimeString()}
+        </div>
+        <button
+          className="comment__list-item--delete-btn"
+          onClick={() => props.deleteComment(id)}
+        >
+          Delete
+        </button>
+
+      </div>
     </li>
   )
 }
